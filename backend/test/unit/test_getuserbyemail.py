753b91d34ba529ba2a1b04d83996fe90b85720d7
Andreas Thiberg <andreas.thiberg@gmail.com>
2023-04-18 12:32:18 +0200

    Added getuserbyemail exception test

--- backend/test/unit/test_getuserbyemail.py
+++ backend/test/unit/test_getuserbyemail.py
@@ -3,14 +3,31 @@ import unittest.mock as mock
 
 from src.controllers.usercontroller import UserController
 
+@pytest.fixture
+def dao_mock():
+    dao_mock = mock.MagicMock()
+    yield dao_mock
+
+
 # Check results when a a matching user is not found in the system.
 @pytest.mark.unit
-def test_getUserByEmail_userNotFound():
-    mockedDAO = mock.MagicMock()
-    mockedDAO.find.return_value = []
+def test_getUserByEmail_userNotFound(dao_mock):
+    dao_mock.find.return_value = []
 
-    sut = UserController(dao=mockedDAO)
+    sut = UserController(dao=dao_mock)
 
     userResult = sut.get_user_by_email(email="testname@email.com")
 
     assert userResult == None
+
+# Test handling of database exception when retrieving user info
+@pytest.mark.unit
+@mock.patch('src.controllers.usercontroller.DAO')
+def test_get_user_by_email_exception(dao_mock):
+    # Mock DAO's find method to raise an exception
+    dao_mock.find = mock.MagicMock(side_effect=Exception('Database error'))
+
+    sut = UserController(dao=dao_mock)
+
+    with pytest.raises(Exception):
+        sut.get_user_by_email(email='testname@email.com')
